f8a769d3914fc68c31c92dabe969db53e3e4ba5b
Tejas Shah <tejasshah2k19@gmail.com>
2025-02-07 19:44:11 +0530

    spinner and toast

--- app/src/main/java/com/royal/androidgentuefri/AddProductActivity.java
+++ app/src/main/java/com/royal/androidgentuefri/AddProductActivity.java
@@ -3,8 +3,13 @@ package com.royal.androidgentuefri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +25,8 @@ public class AddProductActivity extends AppCompatActivity {
     EditText edtName;
     EditText edtQty;
     EditText edtPrice;
+    Spinner spinnerCategory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,12 @@ public class AddProductActivity extends AppCompatActivity {
         edtName = findViewById(R.id.edtAddProductName);
         edtPrice = findViewById(R.id.edtAddProductPrice);
         edtQty = findViewById(R.id.edtAddProductQty);
+        spinnerCategory = findViewById(R.id.spinnerAddProductCategory);
 
+        String categories [] = {"Select Category","MobilePhone","Accessories","HomeAppliances"};
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,categories);
+        spinnerCategory.setAdapter(adapter);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,10 +53,13 @@ public class AddProductActivity extends AppCompatActivity {
                    String productName = edtName.getText().toString();
                    String strPrice = edtPrice.getText().toString();//read -> String
                    String strQty = edtQty.getText().toString();//read ->qty
+                    String category = spinnerCategory.getSelectedItem().toString();
 
                     Log.i("addproduct",productName);
                     Log.i("addproduct",strPrice);
                     Log.i("addproduct",strQty);
+                    Log.i("category",category);
+
 
                     //validation
                     if(productName.isEmpty()){
@@ -59,6 +73,8 @@ public class AddProductActivity extends AppCompatActivity {
                     if(strPrice.isEmpty()){
                         edtPrice.setError("Price Required");
                     }
+
+                Toast.makeText(getApplicationContext(),"Please correct Error(s)",Toast.LENGTH_LONG).show();
 
 
             }
