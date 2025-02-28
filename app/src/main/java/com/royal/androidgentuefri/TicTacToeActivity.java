75d7c4cfa76466f1a30f29fc3fcc4843c631e841
Tejas Shah <tejasshah2k19@gmail.com>
2025-02-28 20:15:10 +0530

    tic tac toe

--- app/src/main/java/com/royal/androidgentuefri/TicTacToeActivity.java
+++ app/src/main/java/com/royal/androidgentuefri/TicTacToeActivity.java
@@ -1,11 +1,13 @@
 package com.royal.androidgentuefri;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +20,20 @@ public class TicTacToeActivity extends AppCompatActivity {
     TextView tvPlayerName;
     Integer player = 1;
     ImageButton imgBtn[] = new ImageButton[9];
+
+    Integer gameState [] = new Integer[9];
+
+    Integer gameCounter = 0;
+
+
+    Integer winningPattern [][] = {
+            {0,1,2},{3,4,5},{6,7,8}
+            ,
+            {0,3,6},{1,4,7},{2,5,8}
+            ,
+            {0,4,8},{2,4,6}
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,28 +60,86 @@ public class TicTacToeActivity extends AppCompatActivity {
         imgBtn[8] = findViewById(R.id.imgBtn9);
 
         for(int i=0;i<imgBtn.length;i++){
-            imgBtn[i].setOnClickListener(this::play);
+            int finalI = i;
+            imgBtn[i].setOnClickListener(view->play(view, finalI +1));
         }
 
 
     }
 
-    private void play(View view) {
+    private void play(View view,int buttonNumber) {
         ImageButton btnClick = findViewById(view.getId());
 
 
         if(btnClick.getBackground().toString().contains("Ripple")){
+
+            gameCounter++;
+
+
             if(player == 1){
+
+                gameState[buttonNumber-1]=0;
                 player=2;
                 btnClick.setBackground(getDrawable(R.drawable.circle));
                 tvPlayerName.setText("Player "+player);
             }else if(player ==2 ){
                 player =1;
+                gameState[buttonNumber-1]=1;
                 btnClick.setBackground(getDrawable(R.drawable.cross));
                 tvPlayerName.setText("Player "+player);
 
             }
+
+            if(gameCounter > 4){
+                Integer status = checkForWinner();
+                if(status == 1){
+                    //winner
+                    Toast.makeText(getApplicationContext(), "Player 2 Win the Game", Toast.LENGTH_SHORT).show();
+                }else if(status == 0){
+                    Toast.makeText(getApplicationContext(), "Player 1 Win the Game", Toast.LENGTH_SHORT).show();
+                }else if(status == -1){
+                    //draw
+                    Toast.makeText(getApplicationContext(), "DRAW", Toast.LENGTH_SHORT).show();
+                }else if(status == 2){
+                    //continue
+                }
+
+                if(status == 1 || status == 0){
+
+                }
+            }
         }
 
     }
+
+    private Integer checkForWinner(){
+
+        for(int i=0,j=0;i<9;i=i+3,j++) {
+            //0 1 2
+            //3 4 5
+            //6 7 8
+            if (gameState[i] == gameState[i+1] && gameState[i+1] == gameState[i+2]) {
+                return gameState[i];//0 1
+            }
+
+            if (gameState[j] == gameState[j+3] && gameState[j+3] == gameState[j+6]) {
+                return gameState[j];//0 1
+            }
+
+
+        }
+        if(gameState[0] == gameState[4] &&  gameState[4] == gameState[8]){
+            return gameState[0];
+        }
+        if(gameState[2] == gameState[4] && gameState[4] == gameState[6]){
+            return gameState[2];
+        }
+        if(gameCounter == 9){
+            return -1; //draw
+        }
+        //win? return 1
+
+        return 2;//no winner
+    }
+
 }
